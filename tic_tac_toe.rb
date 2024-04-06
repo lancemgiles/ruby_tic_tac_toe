5da075d9ec490b88a4e686c175b41851af46020b
Lance <dogmandevices@gmail.com>
2024-04-06 14:09:16 -0500

    allow player to select a cell, update the board. replace board hash with array, remove unneeded module

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,24 +1,12 @@
-# Methods that players use to interact with Gameboard
-module Gameplay
-  def make_move(turn)
-  end
-  # is there a winner?
-  def winner?
-  end
-  
-end
-
-# Game loop
 class Gameboard
-  include Gameplay
   attr_accessor :board, :player, :computer
-  # start a new game (maybe choose computer or 2 player)
+
   def initialize
     @player = {mark: "X", turn: nil}
     @computer = {mark: "O", turn: nil}
     puts "\tLet's play Tic Tac Toe!"
-    @board = {row1: [1, 2, 3], row2: [4, 5, 6], row3: [7, 8, 9]}
-    update_board
+    @board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    show_board
     puts "\tYou are 'X' and the computer is 'O'"
     puts "\tEnter the digit of the area you want to mark"
     first_turn
@@ -28,12 +16,13 @@ class Gameboard
       puts "\tComputer moves first."
     end
   end
-  def update_board
-    @board.each { |key, value|
-      puts "\t\t#{value}"
+
+  def show_board
+    @board.each_slice(3).with_index { |cell, index|
+      puts "\t\t#{cell}"
     }
   end
-  # display board, empty cells have a number
+
   def play_again?
     puts "Play again? Type 'y' for yes"
     reply = gets.chomp
@@ -43,6 +32,7 @@ class Gameboard
       abort "Finished!"
     end
   end
+
   def first_turn
     num = rand(2)
     if num == 1
@@ -53,22 +43,46 @@ class Gameboard
       @computer[:turn] = true
     end
   end
+
   def get_turn
-    if player[:turn] == true
-      turn = gets.chomp
+    if @player[:turn] == true
+      get_player_move
+      @player[:turn] = false
+      @computer[:turn] = true
     else
       turn = get_computer_move
+      @player[:turn] = true
+      @computer[:turn] = false
     end
   end
+
   def get_computer_move
     #determine and select best possible move
+    puts "to do"
+    show_board
   end
-  # repeat until win:
-    # Player/Computer turn
-    # update board
-    # determine winner
-  # Play again?
-end
+  def get_player_move
+    # make sure the player selects an empty (non-integer) cell before returning
+    move = gets.chomp.to_i
+    unless move.is_a? Integer
+      puts "Invalid selection. Please enter a number"
+    end
+    @board[move] = "X"
+    show_board
+  end
 
+  def winner?
+    # check if any win condition met
+    # check who won
+    # display winner
+    play_again?
+  end
+
+end
+# create board
 board = Gameboard.new()
-# board.update_board
+
+while true
+  #board.winner?
+  board.get_turn
+end
