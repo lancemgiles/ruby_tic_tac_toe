bf6ea7cb15fc9f4890b541119f2ff101467c1d6f
Lance <dogmandevices@gmail.com>
2024-04-06 13:02:56 -0500

    initializing the board creates player, computer, board, gets first turn, displays board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,5 @@
 # Methods that players use to interact with Gameboard
 module Gameplay
-  # determine who's turn it is (first turn is random)
-  def get_turn
-
-  end
   def make_move(turn)
   end
   # is there a winner?
@@ -15,14 +11,16 @@ end
 # Game loop
 class Gameboard
   include Gameplay
-  attr_accessor :board, :player, :computer, :turn
+  attr_accessor :board, :player, :computer
   # start a new game (maybe choose computer or 2 player)
   def initialize
     @player = {mark: "X", turn: nil}
     @computer = {mark: "O", turn: nil}
     puts "\tLet's play Tic Tac Toe!"
     @board = {row1: [1, 2, 3], row2: [4, 5, 6], row3: [7, 8, 9]}
+    update_board
     puts "\tYou are 'X' and the computer is 'O'"
+    puts "\tEnter the digit of the area you want to mark"
     first_turn
     if @player[:turn] == true
       puts "\tYou start."
@@ -55,32 +53,21 @@ class Gameboard
       @computer[:turn] = true
     end
   end
+  def get_turn
+    if player[:turn] == true
+      turn = gets.chomp
+    else
+      turn = get_computer_move
+    end
+  end
+  def get_computer_move
+    #determine and select best possible move
+  end
   # repeat until win:
     # Player/Computer turn
     # update board
     # determine winner
   # Play again?
-end
-
-class Player
-  include Gameplay
-  attr_accessor :player
-  def initialize
-    @player = "X"
-  end
-  #
-end
-
-class Computer
-  include Gameplay
-  attr_accessor :computer
-  def initialize
-    @computer = "O"
-  end
-  def analyze_board
-    # determine possible moves
-    # return best move
-  end
 end
 
 board = Gameboard.new()
